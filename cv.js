105d8a8ba58927863310a68dab1cee937e1d0ce0
karusodcm <karlsdicks@gmail.com>
2024-10-17 17:14:31 -0500

    Add files via upload

---
+++ cv.js
@@ -0,0 +1,9 @@
+const cdownload = document.getElementById('cdownload');
+const download = document.getElementById('download');
+const close = document.getElementById('close');
+
+close.addEventListener('click',()=>{
+    cdownload.style.display ='none'
+})
+
+
